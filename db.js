090513d994f067d7fc519013c1960cf1eb602bcd
Sathishsooraa2312 <sathishrohin22@gmail.com>
2023-02-09 23:33:51 +0530

    second commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const url =
-"mongodb+srv://sathishkumar:Sathish123@cluster0.ass1yz7.mongodb.net";
+process.env.MONGO_URL;
 module.exports.connect = () => {
   mongoose
     .connect(url, {
